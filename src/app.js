51eb311c4d17aa429c7cf082e9bb527082b5a511
miller-203 <youneslaydi7@gmail.com>
2025-07-04 18:16:18 +0100

    waiting fix app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ export function createApp({ state, view, reducers = {} }) {
     for (const actionName in reducers) {
         const reducer = reducers[actionName];
         const subs = dispatcher.subscribe(actionName, payload => {
+            const newState = reducer(state, payload);
+            // if (newState !== state) {
+            //     state == newState;
+            // }
             state = reducer(state, payload);
         });
         subscriptions.push(subs);
